783066cc4e939dd4a310dad16151e089816ad0fa
Kauan Gomes Cardoso <kauangomescardoso100@gmail.com>
2025-07-10 22:14:36 -0300

    TESTE

--- trabalho_2_01/src/main/java/jv/kauan/trabalho_1_01/testes.java
+++ trabalho_2_01/src/main/java/jv/kauan/trabalho_1_01/testes.java
@@ -5,5 +5,8 @@ import jv.kauan.trabalho_1_01.interfaces.MainWindow;
 public class testes {
     public static void main(String[] args) {
         new MainWindow();
+        //jabuti
+        int jabuti;
+        System.out.println("teste");
     }
 }
